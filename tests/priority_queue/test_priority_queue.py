2687c9d743cfbe2d71f54b57142f5e569f2badeb
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-08-07 13:31:47 -0300

    requisito 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,29 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    assert len(queue) == 0
+
+    list_elements = [
+        {"qtd_linhas": 20, "nome": "file1.txt"},
+        {"qtd_linhas": 3, "nome": "file3.txt"},
+        {"qtd_linhas": 230, "nome": "file2.txt"},
+    ]
+    for element in list_elements:
+        queue.enqueue(element)
+    assert len(queue) == 3
+    assert len(queue.high_priority) == 1
+    assert len(queue.regular_priority) == 2
+
+    with pytest.raises(IndexError, match='Índice Inválido ou Inexistente'):
+        queue.search(5)
+    assert queue.search(0) == list_elements[1]
+    assert queue.search(1) == list_elements[0]
+    assert queue.search(2) == list_elements[2]
+
+    queue.dequeue()
+    assert queue.search(0) == list_elements[0]
+    queue.dequeue()
+    assert queue.search(0) == list_elements[2]
