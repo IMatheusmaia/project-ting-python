80ab77495bf3c9736d0366a9f0adb0480e457875
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-08-07 15:58:37 -0300

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for index in range(len(instance)):
+        file_meta_data = instance.search(index)
+        file_name = file_meta_data["nome_do_arquivo"]
+        file_lines = file_meta_data["linhas_do_arquivo"]
+
+        occurrences = [
+            {"linha": line_num + 1}
+            for line_num, line in enumerate(file_lines)
+            if word.lower() in line.lower()
+        ]
+
+        if occurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            })
+
+    return results
 
 
 def search_by_word(word, instance):
